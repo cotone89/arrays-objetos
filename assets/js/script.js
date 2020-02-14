357319ce6293062731f576f478d96ce2e2015d61
Maria Jose Tamayo <ma.jose.tamayo89@gmail.com>
2020-02-14 16:19:11 -0300

    desafio ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,43 +88,6 @@ var traumatologia = [
     }
 ];
 
-var radiologia = [
-    {
-        hora: "11:00",
-        especialista: "IGNACIO SCHULZ",
-        paciente: "FRANCISCA ROJAS",
-        rut: "9878782-1",
-        prevision: "FONASA"
-    },
-    {
-        hora: "11:30",
-        especialista: "FEDERICO SUBERCASEAUX",
-        paciente: "PAMELA ESTRADA",
-        rut: "15345241-3",
-        prevision: "ISAPRE"
-    },
-    {
-        hora: "15:00",
-        especialista: "FERNANDO WURTHZ",
-        paciente: "ARMANDO LUNA",
-        rut: "16445345-9",
-        prevision: "ISAPRE"
-    },
-    {
-        hora: "15:30",
-        especialista: "ANA MARIA GODOY",
-        paciente: "MANUEL GODOY",
-        rut: "17666419-0",
-        prevision: "FONASA"
-    },
-    {
-        hora: "16:00",
-        especialista: "PATRICIA SUAZO",
-        paciente: "RAMON ULLOA",
-        rut: "14989389-K",
-        prevision: "FONASA"
-    }
-];
 
 var dental = [
     {
@@ -171,7 +134,49 @@ var dental = [
     }
 ];
 
+var consultasRadiologia = radiologia.length;
+var consultasTraumatologia = traumatologia.length;
+var consultasDental = dental.length;
+
+//Radiologia
+var primeraRadiologiaPaciente = radiologia[0].paciente;
+var primeraRadiologiaRut = radiologia[0].rut;
+var primeraRadiologiaPrevision = radiologia[0].prevision;
+
+var ultimaRadiologiaPaciente = radiologia[radiologia.length-1].paciente;
+var ultimaRadiologiaRut = radiologia[radiologia.length-1].rut;
+var ultimaRadiologiaPrevision = radiologia[radiologia.length-1].prevision;
+
+//Traumatologia
+var primeraTraumatologiaPaciente = traumatologia[0].paciente;
+var primeraTraumatologiaRut = traumatologia[0].rut;
+var primeraTraumatologiaPrevision = traumatologia[0].prevision;
+
+var ultimaTraumatologiaPaciente = traumatologia[traumatologia.length - 1].paciente;
+var ultimaTraumatologiaRut = traumatologia[traumatologia.length - 1].rut;
+var ultimaTraumatologiaPrevision = traumatologia[traumatologia.length - 1].prevision;
+
+//Dental
+var primeraDentalPaciente = dental[0].paciente;
+var primeraDentalRut = dental[0].rut;
+var primeraDentalPrevision = dental[0].prevision;
+
+var ultimaDentalPaciente = dental[dental.length - 1].paciente;
+var ultimaDentalRut = dental[dental.length - 1].rut;
+var ultimaDentalPrevision = dental[dental.length - 1].prevision;
 
 
 
-document.write('<p>Estad&iacute;sticas centro m&eacute;dico &ntilde;u&ntilde;oa</p>');
+document.write('<h1>Estad&iacute;sticas centro m&eacute;dico &Ntilde;u&ntilde;oa</h1>');
+
+document.write('<p class="styleSub">Consultas de Radiolog&iacute;a: ' + consultasRadiologia + '</p>');
+document.write('<p>Primer paciente Radiolog&iacute;a: ' + primeraRadiologiaPaciente + ' - ' + primeraRadiologiaRut + ' - ' + primeraRadiologiaPrevision+'</p>');
+document.write('<p>&Uacute;ltimo paciente Radiolog&iacute;a: ' + ultimaRadiologiaPaciente + ' - ' + ultimaRadiologiaRut + ' - ' + ultimaRadiologiaPrevision + '</p><br/>');
+
+document.write('<p class="styleSub">Consultas de Traumatolog&iacute;a: ' + consultasTraumatologia + '</p>');
+document.write('<p>Primer paciente Traumatolog&iacute;a: ' + primeraTraumatologiaPaciente + ' - ' + primeraTraumatologiaRut + ' - ' + primeraTraumatologiaPrevision + '</p>');
+document.write('<p>&Uacute;ltimo paciente Traumatolog&iacute;a: ' + ultimaTraumatologiaPaciente + ' - ' + ultimaTraumatologiaRut + ' - ' + ultimaTraumatologiaPrevision + '</p><br/>');
+
+document.write('<p class="styleSub">Consultas Dentales: ' + consultasDental + '</p>');
+document.write('<p>Primer paciente Dental: ' + primeraDentalPaciente + ' - ' + primeraDentalRut + ' - ' + primeraDentalPrevision + '</p>');
+document.write('<p>&Uacute;ltimo paciente Dental: ' + ultimaDentalPaciente + ' - ' + ultimaDentalRut + ' - ' + ultimaDentalPrevision + '</p><br/>');
